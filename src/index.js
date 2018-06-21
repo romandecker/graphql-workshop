773b6ecee3d673f6fa7ad08f7f671f4192d2254b
Roman Decker <Roman.Decker@zooplus.com>
2018-06-21 14:33:02 +0200

    :sparkles: Serve graphql via express

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import 'source-map-support/register';
 import { graphql } from 'graphql';
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
 
 import schema from './lib/schema';
 
-async function main() {
-  // use graphql here!
-}
+const app = express();
 
-main();
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema
+  })
+);
+
+app.listen(7777, () => console.log('GraphQL endpoint listening on http://localhost:7777/graphql'));
