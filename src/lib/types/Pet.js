edb5c656294c68c2d36f219f9e3758bd06b4d916
Roman Decker <Roman.Decker@zooplus.com>
2018-06-27 13:01:49 +0200

    :sparkles: Add queries

--- src/lib/types/Pet.js
+++ src/lib/types/Pet.js
@@ -6,12 +6,26 @@ import {
     GraphQLList,
 } from 'graphql';
 
-import Species from './Species';
+import { getToysByPet, getFriendsByPet } from '../db';
 
-export default new GraphQLObjectType({
+import Species from './Species';
+import Toy from './Toy';
+
+const Pet = new GraphQLObjectType({
     name: 'Pet',
-    fields: {
+    fields: () => ({
+        id: { type: new GraphQLNonNull(GraphQLString) },
         name: { type: new GraphQLNonNull(GraphQLString) },
         species: { type: new GraphQLNonNull(Species) },
-    },
+        friends: {
+            type: new GraphQLNonNull(new GraphQLList(Pet)),
+            resolve: (pet) => getFriendsByPet(pet),
+        },
+        toysPlayedWith: {
+            type: new GraphQLNonNull(new GraphQLList(Toy)),
+            resolve: (pet) => getToysByPet(pet),
+        },
+    }),
 });
+
+export default Pet;
