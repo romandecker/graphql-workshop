70c973bca135826d7b7b72e62fb87bb9658b722f
Roman Decker <Roman.Decker@zooplus.com>
2018-06-27 13:01:49 +0200

    :sparkles: Add queries

--- src/lib/schema.js
+++ src/lib/schema.js
@@ -1,12 +1,39 @@
-import { GraphQLSchema, GraphQLObjectType, GraphQLNonNull, GraphQLString } from 'graphql';
+import {
+    GraphQLSchema,
+    GraphQLObjectType,
+    GraphQLNonNull,
+    GraphQLList,
+    GraphQLString,
+} from 'graphql';
+import Pet from './types/Pet';
+import Toy from './types/Toy';
+import { getPets, getPetById, getToys, getToyById } from './db';
 
 export default new GraphQLSchema({
     query: new GraphQLObjectType({
         name: 'Query',
         fields: {
-            greet: {
-                type: new GraphQLNonNull(GraphQLString),
-                resolve: () => 'Hey there!',
+            pets: {
+                type: new GraphQLNonNull(new GraphQLList(Pet)),
+                resolve: () => getPets(),
+            },
+            petById: {
+                type: Pet,
+                args: {
+                    id: { type: new GraphQLNonNull(GraphQLString) },
+                },
+                resolve: (parent, args) => getPetById(args.id),
+            },
+            toys: {
+                type: new GraphQLNonNull(new GraphQLList(Toy)),
+                resolve: () => getToys(),
+            },
+            toyById: {
+                type: Toy,
+                args: {
+                    id: { type: new GraphQLNonNull(GraphQLString) },
+                },
+                resolve: (parent, args) => getToyById(args.id),
             },
         },
     }),
