ec4ee0dd6b96b0a4203e23a8ce2e1f956fc9f9b5
Roman Decker <Roman.Decker@zooplus.com>
2018-06-27 12:19:01 +0200

    :sparkles: Add more data

---
+++ src/lib/db.js
@@ -0,0 +1,19 @@
+import keyBy from 'lodash/fp/keyBy';
+import { pets, toys } from './data';
+
+const keyById = keyBy('id');
+
+const petsById = keyById(pets);
+const toysById = keyById(toys);
+
+export const getPets = () => pets;
+export const getToys = () => toys;
+
+const createSubitemGetter = (subitemsById, subitemsIdsPropertyName) => (item) =>
+    item[subitemsIdsPropertyName].map((id) => subitemsById[id]);
+
+export const getFriendsByPet = createSubitemGetter(petsById, 'friendIds');
+export const getPetById = (id) => petsById[id];
+
+export const getToyById = (id) => toysById[id];
+export const getToysByPet = createSubitemGetter(toysById, 'toysPlayedWithIds');
