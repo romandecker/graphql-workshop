ecd4e54d218c42bec36c81b6e2b8868b115b8907
Roman Decker <Roman.Decker@zooplus.com>
2018-06-27 14:35:26 +0200

    :sparkles: End of workshop

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,6 +4,27 @@ import ApolloClient from 'apollo-boost';
 import { ApolloProvider, Query, Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
+const client = new ApolloClient({
+    uri: '/graphql',
+});
+
+const GET_TOYS = gql`
+    {
+        toys {
+            id
+            name
+        }
+    }
+`;
+
+const ADD_TOY = gql`
+    mutation($toyData: ToyInput!) {
+        createToy(toyData: $toyData) {
+            name
+        }
+    }
+`;
+
 class App extends Component {
     constructor(props) {
         super(props);
@@ -16,20 +37,49 @@ class App extends Component {
 
     render() {
         return (
-            <div className="App">
-                Toys:
-                <ul>
-                    <li>No toys :/</li>
-                </ul>
-                <div>
-                    <input
-                        type="text"
-                        value={this.state.toyName}
-                        onChange={this.handleToyNameChange}
-                    />
-                    <button>Add toy</button>
+            <ApolloProvider client={client}>
+                <div className="App">
+                    Toys:
+                    <ul>
+                        <Query query={GET_TOYS}>
+                            {({ data }) => {
+                                const toys = data.toys;
+                                if (toys) {
+                                    return toys.map((toy) => <li key={toy.id}>{toy.name}</li>);
+                                }
+
+                                return <li>No toys :/</li>;
+                            }}
+                        </Query>
+                    </ul>
+                    <div>
+                        <input
+                            type="text"
+                            value={this.state.toyName}
+                            onChange={this.handleToyNameChange}
+                        />
+                        <Mutation mutation={ADD_TOY} refetchQueries={[{ query: GET_TOYS }]}>
+                            {(createToy) => (
+                                <button
+                                    onClick={() =>
+                                        createToy({
+                                            variables: {
+                                                toyData: {
+                                                    name: this.state.toyName,
+                                                    ownerId: 'aadeadd5-82ad-4000-bae6-4b849a82db51',
+                                                    weight: 100,
+                                                },
+                                            },
+                                        })
+                                    }
+                                >
+                                    Add toy
+                                </button>
+                            )}
+                        </Mutation>
+                    </div>
                 </div>
-            </div>
+            </ApolloProvider>
         );
     }
 }
