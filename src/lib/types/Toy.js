19401f5badc31c2990086b28858367845d569845
Roman Decker <Roman.Decker@zooplus.com>
2018-06-27 13:01:49 +0200

    :sparkles: Add queries

--- src/lib/types/Toy.js
+++ src/lib/types/Toy.js
@@ -1,9 +1,17 @@
 import { GraphQLObjectType, GraphQLNonNull, GraphQLString, GraphQLInt } from 'graphql';
 
+import Pet from './Pet';
+import { getPetById } from '../db';
+
 export default new GraphQLObjectType({
     name: 'Toy',
-    fields: {
+    fields: () => ({
+        id: { type: new GraphQLNonNull(GraphQLString) },
         name: { type: new GraphQLNonNull(GraphQLString) },
         weight: { type: new GraphQLNonNull(GraphQLInt) },
-    },
+        owner: {
+            type: new GraphQLNonNull(Pet),
+            resolve: (toy) => getPetById(toy.ownerId),
+        },
+    }),
 });
